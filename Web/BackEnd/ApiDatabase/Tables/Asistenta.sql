c561d9825fbf1f07636a23b5bea794eda0c0c91c
Popushka <emanuel.oltean12@gmail.com>
2021-04-26 12:28:06 +0300

    Created SQL tables

---
+++ Web/BackEnd/ApiDatabase/Tables/Asistenta.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Asistenta]
+(
+	[AsistentaId] INT NOT NULL, 
+    [Nume] NVARCHAR(50) NOT NULL, 
+    [Prenume] NVARCHAR(50) NOT NULL, 
+    [NumeUtilizator] NVARCHAR(50) NOT NULL, 
+    [Parola] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [PK_AsistentaId] PRIMARY KEY ([AsistentaId]) 
+)
