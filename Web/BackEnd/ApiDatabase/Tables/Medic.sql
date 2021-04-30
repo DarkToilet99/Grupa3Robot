e1b29dc7042700138cba7d9767862576a36f8a59
Stellrow <42060342+Stellrow@users.noreply.github.com>
2021-04-30 21:21:32 +0300

    Delete Web/BackEnd/ApiDatabase directory

--- Web/BackEnd/ApiDatabase/Tables/Medic.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Medic]
-(
-	[MedicId] INT NOT NULL Identity(1,1), 
-    [Nume] NVARCHAR(50) NOT NULL, 
-    [Prenume] NVARCHAR(50) NOT NULL, 
-    [CodParafa] INT NOT NULL, 
-    [Parola] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [PK_MedicId] PRIMARY KEY ([MedicId])
-)
--- Web/BackEnd/ApiDatabase/Tables/Transport.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Transport]
-(
-	[TransportId] INT NOT NULL PRIMARY KEY, 
-    [RobotId] INT NOT NULL, 
-    [Data] DATETIME NOT NULL, 
-    [Efectuat] BIT NOT NULL, 
-    CONSTRAINT [FK_Transport_RobotId] FOREIGN KEY ([RobotId]) REFERENCES [Robot]([RobotId])
-)
