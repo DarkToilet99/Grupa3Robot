e8bfc18385a527ab07af48f971babad5ca6e7947
BuildTools <unconfigured@null.spigotmc.org>
2021-04-30 21:32:07 +0300

    Fixed folder merging

---
+++ Web/Backend/ApiDatabase/Tables/Transport.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Transport]
+(
+	[TransportId] INT NOT NULL PRIMARY KEY, 
+    [RobotId] INT NOT NULL, 
+    [Data] DATETIME NOT NULL, 
+    [Efectuat] BIT NOT NULL, 
+    CONSTRAINT [FK_Transport_RobotId] FOREIGN KEY ([RobotId]) REFERENCES [Robot]([RobotId])
+)
